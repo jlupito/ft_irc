53b5765de153534640c78c2ed83665c2b2fb909b
Alix Bourboulon <alix.bourboulon@gmail.com>
2024-02-08 16:42:12 +0100

    before forking

--- Server.hpp
+++ Server.hpp
@@ -1,23 +1,15 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Server.hpp                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jarthaud <jarthaud@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/08 12:04:08 by jarthaud          #+#    #+#             */
-/*   Updated: 2024/02/08 12:05:58 by jarthaud         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #pragma once
 #include <iostream>
 
 class Server {
-	private:
+
 	public:
 		Server();
 		~Server();
 		Server(const Server &copy);
 		Server& operator=(const Server &copy);
+
+	private:
+
+
 };
---
+++ User.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+
+class User {
+
+	public:
+		User();
+		~User();
+		User(const User &copy);
+		User& operator=(const User &copy);
+
+	private:
+	std::string
+
+};
