dd698e2e519230e3a315f49df65d8db8ed97abf8
jlupito <jl.arthaud@protonmail.com>
2024-02-27 17:04:27 +0100

    working on modes

--- Command_MODE.cpp
+++ Command_MODE.cpp
@@ -46,86 +46,114 @@ void handleMODECommand(Server& server, Client* client, cmdStruct* command) {
 	if (handleModeErrors(client, channel, user, command))
 		return;
 
-	int remove = 0;
 	if (command->params[2][0] != '-' and command->params[2][0] != '+')
 		return ;
-	else if (command->params[2][0] != '-')
-		remove = 1;
-	std::string modeparam = command->params[4];
-	switch (command->params[2][1]) {
-		case ('o'):
-			if (remove) {
-				if (channel->isClient(modeparam) and channel->isOperator(modeparam))
-					channel->removeOperator(modeparam);
-			}
-			else {
-				if (channel->isClient(modeparam) and !channel->isOperator(modeparam))
-					channel->addOperators(modeparam);
-			}
-			break ;
 
-		case ('t'):
-			if (remove) {
-				if (channel->getMode().find("t") != std::string::npos)
-					channel->removeMode("t");
-			}
-			else {
-				if (channel->getMode().find("t") == std::string::npos)
-					channel->addMode("t");
-			}
-			break ;
-		case ('i'):
-			if (remove) {
-				if (channel->getMode().find("i") != std::string::npos)
-					channel->removeMode("i");
-			}
-			else {
-				if (channel->getMode().find("i") == std::string::npos)
-					channel->addMode("i");
-			}
-			break ;
+	int remove;
+	std::vector<std::string> modeParams;
+	int nbParams = command->params.size() - 3;
 
-		case ('l'):
-			if (remove) {
-				if (channel->getMode().find("l") != std::string::npos)
-					channel->removeMode("l");
-			}
-			else {
-				if (channel->getMode().find("l") != std::string::npos) {
-					std::istringstream iss(modeparam);
-    				int nb;
-					if (iss >> nb) {
-						channel->setNbrUsersLimit(nb);
-						channel->addMode("l");
-					}
-				}
-			}
-			break ;
+	for (size_t i = 0; i <= nbParams; i++)
+		modeParams.push_back(command->params[i]);
+	std::vector<std::string>::iterator itParam = modeParams.begin();
 
-		case ('k'):
-			if (remove) {
-				if (channel->getMode().find("k") != std::string::npos) {
-					channel->removeMode("k");
-					if (!channel->getChannelPwd().empty())
-						channel->getChannelPwd().clear();
-				}
-			}
-			else {
-				if (channel->getMode().find("k") == std::string::npos) {
-					channel->addMode("k");
-					channel->setChannelPwd(modeparam);
+	for (size_t i = 0; command->params[2][i]; i++) {
+
+		switch (command->params[2][i]) {
+			case ('-'):
+				remove = 1;
+				break;
+
+			case ('+'):
+				remove = 0;
+				break;
+
+			case ('o'):
+				if (remove) {
+					if (channel->isClient(*itParam) and channel->isOperator(*itParam))
+						channel->removeOperator(*itParam);
 				}
 				else {
-					reply = KEYSET_ERR(channelName);
-					sendBytesToClient(client, reply.c_str());
+					if (channel->isClient(*itParam) and !channel->isOperator(*itParam))
+						channel->addOperators(*itParam);
 				}
-			}
-			break ;
+				if (++itParam != modeParams.end())
+					itParam++;
+				else 
+					*itParam == "";
+				break ;
 
-		default :
-			reply = UNKNOWNMODE_ERR(command->params[2][1], channelName);
-			sendBytesToClient(client, reply.c_str());
-			break;
+			case ('t'):
+				if (remove) {
+					if (channel->getMode().find("t") != std::string::npos)
+						channel->removeMode("t");
+				}
+				else {
+					if (channel->getMode().find("t") == std::string::npos)
+						channel->addMode("t");
+				}
+				break ;
+			case ('i'):
+				if (remove) {
+					if (channel->getMode().find("i") != std::string::npos)
+						channel->removeMode("i");
+				}
+				else {
+					if (channel->getMode().find("i") == std::string::npos)
+						channel->addMode("i");
+				}
+				break ;
+
+			case ('l'):
+				if (remove) {
+					if (channel->getMode().find("l") != std::string::npos)
+						channel->removeMode("l");
+				}
+				else {
+					if (channel->getMode().find("l") != std::string::npos) {
+						std::istringstream lim(*itParam);
+						int nb;
+						if (lim >> nb) {
+							channel->setNbrUsersLimit(nb);
+							channel->addMode("l");
+						}
+					}
+					if (++itParam != modeParams.end())
+						itParam++;
+					else 
+						*itParam == "";
+				}
+				break ;
+
+			case ('k'):
+				if (remove) {
+					if (channel->getMode().find("k") != std::string::npos) {
+						channel->removeMode("k");
+						if (!channel->getChannelPwd().empty())
+							channel->getChannelPwd().clear();
+					}
+				}
+				else {
+					if (channel->getMode().find("k") == std::string::npos) {
+						channel->addMode("k");
+						channel->setChannelPwd(*itParam);
+						if (++itParam != modeParams.end())
+							itParam++;
+						else 
+							*itParam == "";
+					}
+					else {
+						reply = KEYSET_ERR(channelName);
+						sendBytesToClient(client, reply.c_str());
+					}
+				}
+				break ;
+
+			default :
+				reply = UNKNOWNMODE_ERR(command->params[2][1], channelName);
+				sendBytesToClient(client, reply.c_str());
+				break;
+		}
 	}
 	return ;
 }
