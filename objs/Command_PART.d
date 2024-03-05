cc4578c352c8da2a39b3302dca727549b7ed273e
Alix Bourboulon <alix.bourboulon@gmail.com>
2024-03-05 10:42:36 +0100

    modif nick

---
+++ objs/Command_PART.d
@@ -0,0 +1,12 @@
+objs/Command_PART.o: Command_PART.cpp Server.hpp Client.hpp Commands.hpp \
+  Channel.hpp Replies.hpp
+
+Server.hpp:
+
+Client.hpp:
+
+Commands.hpp:
+
+Channel.hpp:
+
+Replies.hpp:
---
+++ objs/Signals.d
@@ -0,0 +1,12 @@
+objs/Signals.o: Signals.cpp Signals.hpp Server.hpp Client.hpp \
+  Commands.hpp Channel.hpp
+
+Signals.hpp:
+
+Server.hpp:
+
+Client.hpp:
+
+Commands.hpp:
+
+Channel.hpp:
